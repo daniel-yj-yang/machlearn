fba72801b56c462bfdb0ef1444e6aafd6f06f8f6
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-15 15:23:11 -0500

    Update

--- machlearn/__about__.py
+++ machlearn/__about__.py
@@ -9,7 +9,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from machlearn.__about__ import (
-    __version__,
-)
+ 
+__version__ = "0.0.10"
--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/#setup-py
 # https://github.com/pypa/sampleproject/blob/master/setup.py
 
@@ -21,7 +33,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         # https://pypi.org/classifiers/
-        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
