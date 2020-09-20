9d189e13ff311bf453c636149843dc982e7b492d
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-20 02:11:58 -0500

    Update __init__.py

--- machlearn/naive_bayes/__init__.py
+++ machlearn/naive_bayes/__init__.py
@@ -1,14 +1,8 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# -*- coding: utf-8 -*-
+
+# Author: Daniel Yang <daniel.yj.yang@gmail.com>
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# License: BSD 3 clause
 
 from ._naive_bayes import naive_bayes_Bernoulli, naive_bayes_multinomial, naive_bayes_Gaussian, demo
 
