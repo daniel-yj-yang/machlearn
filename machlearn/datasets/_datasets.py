6a4c3e67d441b7b41c25622852a6fb4b1689f9a7
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-23 11:01:21 -0500

    Update

--- machlearn/datasets/_datasets.py
+++ machlearn/datasets/_datasets.py
@@ -8,33 +8,33 @@
 # https://huggingface.co/docs/datasets/
 # https://scikit-learn.org/stable/datasets/index.html#general-dataset-api
 
-from io import BytesIO
+import io
 from zipfile import ZipFile
 import urllib.request
 import csv
 
+import pkgutil
+
 import pandas as pd
 
-class dataset():
+def dataset(dataset_name=None):
     """
-    This class provides an interface for accessing dataset that is publicly available in the Internet.
-    """
-    def __init__(self, dataset_name=None):
-        """
-        dataset_name can be the following:
+    dataset_name can be the following:
 
-            - SMS_spam
-            - Social_Network_Ads
-        """
-        if self.dataset_name is None:
-            raise TypeError('dataset name is not provided')
-        if self.dataset_name == 'SMS_spam':
-            url = urllib.request.urlopen("https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip")
-            self.df = pd.read_csv(ZipFile(BytesIO(url.read())).open('SMSSpamCollection'), sep='\t', quoting=csv.QUOTE_NONE, names=("label", "message"))
-        if self.dataset_name == 'Social_Network_Ads':
-            url = urllib.request.urlopen("https://github.com/daniel-yj-yang/Machine-Learning-using-Python/raw/master/Decision-Tree_classification/Social_Network_Ads.csv")
-            self.df = pd.read_csv(BytesIO(url.read()), encoding='utf8', sep=",")
-        return self.df
+        - SMS_spam
+        - Social_Network_Ads
+    """
+    if dataset_name is None:
+        raise TypeError('dataset name is not provided')
+    if dataset_name == 'SMS_spam':
+        df = pd.read_csv(io.BytesIO(pkgutil.get_data(__name__, "public/SMS_spam_collection/SMSSpamCollection")), sep='\t', quoting=csv.QUOTE_NONE, names=("label", "message"))
+        #url = urllib.request.urlopen("https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip")
+        #df = pd.read_csv(ZipFile(io.BytesIO(url.read())).open('SMSSpamCollection'), sep='\t', quoting=csv.QUOTE_NONE, names=("label", "message"))
+    if dataset_name == 'Social_Network_Ads':
+        df = pd.read_csv(io.BytesIO(pkgutil.get_data(__name__, "public/Social_Network_Ads/Social_Network_Ads.csv")), encoding='utf8', sep=",")
+        #url = urllib.request.urlopen("https://github.com/daniel-yj-yang/machlearn/raw/master/machlearn/datasets/public/Social_Network_Ads.csv")
+        #df = pd.read_csv(io.BytesIO(url.read()), encoding='utf8', sep=",")
+    return df
 
     
 
