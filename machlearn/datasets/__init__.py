faae375513b0439c05030e2e33c0b95fba2083ac
Daniel Yang <daniel.yj.yang@gmail.com>
2020-09-23 12:05:47 -0500

    Update __init__.py

--- machlearn/datasets/__init__.py
+++ machlearn/datasets/__init__.py
@@ -4,7 +4,7 @@
 #
 # License: BSD 3 clause
 
-from ._datasets import dataset
+from ._datasets import public_dataset
 
 # this is for "from <package_name>.datasets import *"
-__all__ = ["dataset"]
+__all__ = ["public_dataset"]
