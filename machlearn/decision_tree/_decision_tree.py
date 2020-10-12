a6061262ce38194a1cde42b1099ca260705044c2
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2020-10-12 15:45:23 -0500

    Update _decision_tree.py

--- machlearn/decision_tree/_decision_tree.py
+++ machlearn/decision_tree/_decision_tree.py
@@ -4,11 +4,23 @@
 #
 # License: BSD 3 clause
 
-def boost():
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+
+
+def boosting(type = "GBM"):
     """
-    boosting
+    Idea: one is weak, together is strong, iterative training leads to the best model.
+    Strategy: To improve the predictive power by training a sequence of weak models. Each additional weak model is to compensate the weaknesses of its predecessors.
+
+    type: (1) GBM = Gradient Boosting Machines, including XGBOOST;
+          (2) Adaptive = AdaBoost
+
+    Details:
+    (2) AdaBoost: at every step the sample distribution was adapted to put more weight on misclassified samples and less weight on correctly classified samples.
     """
-    
+    #return GradientBoostingClassifier(*args, **kwargs)
+    return AdaBoostClassifier(*args, **kwargs)
+
 
 def demo():
     """
