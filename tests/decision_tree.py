717ca768623e3b36fa1799fd14c39914312892af
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2020-10-13 15:48:06 -0500

    Update decision_tree.py

--- tests/decision_tree.py
+++ tests/decision_tree.py
@@ -6,7 +6,5 @@
 
 from machlearn import decision_tree as DT
 
-DT.demo(dataset="Social_Network_Ads", classifier_func="decision_tree")
-DT.demo(dataset="Social_Network_Ads", classifier_func="GBM")
-
-
+DT.demo(dataset = "Social_Network_Ads", classifier_func = "decision_tree")
+DT.demo(dataset = "Social_Network_Ads", classifier_func = "GBM")
