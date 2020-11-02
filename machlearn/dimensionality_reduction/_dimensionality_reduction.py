822feeb01c3f463b5eb2ff6e3199e02cee072f24
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2020-11-01 20:50:46 -0600

    Update

--- machlearn/dimensionality_reduction/_dimensionality_reduction.py
+++ machlearn/dimensionality_reduction/_dimensionality_reduction.py
@@ -5,6 +5,21 @@
 # License: BSD 3 clause
 
 
+from sklearn.decomposition import PCA, FastICA
+from sklearn.cross_decomposition import CCA
+
+
+def principal_component_analysis(*args, **kwargs):
+    return PCA(*args, **kwargs)
+
+
+def independent_component_analysis(*args, **kwargs):
+    return FastICA(*args, **kwargs)
+
+
+def canonical_correlation_analysis(*args, **kwargs):
+    return CCA(*args, **kwargs)
+
 
 def demo():
     pass
